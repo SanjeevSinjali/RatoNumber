23eab3df6012f36ba13ff74bfb053407ca17008f
SanjeevSinjali <tdkzbiru@gmail.com>
2025-06-29 13:37:22 +0545

    Forgot Password Code ADDED

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage';
 import Login from './components/Login';
 import Register from './components/Register';
+import Forgot from './components/Forgot';
 import './App.css';
 
 function App() {
@@ -13,6 +14,8 @@ function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/forgot-password" element={<Forgot />} />
+          {/* You can replace the Login component with a Forgot component if you create one */}
         </Routes>
       </div>
     </Router>
